4ce5cef2fdee80194a0a782c79c10fe7fc6987bc
Parth Narkhede <parth.narkhede@centralogic.net>
2025-01-03 11:53:46 +0530

    initial commit

--- lib/features/crash_analytics/presentation/pages/crash_analytics_view.dart
+++ lib/features/crash_analytics/presentation/pages/crash_analytics_view.dart
@@ -1,8 +1,8 @@
+import 'package:firebase_demo/utils/logging.dart';
 import 'package:flutter/material.dart';
-import 'utils/logging.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class CrashAnalyticsView extends StatelessWidget {
+  const CrashAnalyticsView({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,40 @@
 import 'dart:ui';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
-import 'home.dart';
+import 'features/crash_analytics/presentation/pages/crash_analytics_view.dart';
 import 'utils/logging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
-  // Set up error handling for both Flutter errors and platform-specific errors
-  FlutterError.onError = (FlutterErrorDetails details) {
-    // Send the error to Firebase Crashlytics
-    FirebaseCrashlytics.instance.recordFlutterFatalError(details);
-  };
+  try {
+    // Initialize Firebase with the platform-specific options
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
-  // Catch platform errors (e.g., Android or iOS-specific errors)
-  PlatformDispatcher.instance.onError = (error, stack) {
-    // Send the error to Firebase Crashlytics with custom message
-    logErrorWithNotes(error, stack, 'Platform-level error occurred');
-    return true; // Return true to prevent Flutter from reporting the error
-  };
-  FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+    // Set up error handling for both Flutter errors and platform-specific errors
+    FlutterError.onError = (FlutterErrorDetails details) {
+      // Send the error to Firebase Crashlytics
+      FirebaseCrashlytics.instance.recordFlutterFatalError(details);
+    };
+
+    // Catch platform errors (e.g., Android or iOS-specific errors)
+    PlatformDispatcher.instance.onError = (error, stack) {
+      // Send the error to Firebase Crashlytics with a custom message
+      logErrorWithNotes(error, stack, 'Platform-level error occurred');
+      return true; // Return true to prevent Flutter from reporting the error
+    };
+
+    FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  } catch (e, stackTrace) {
+    // Handle any error that occurs during Firebase initialization
+    logErrorWithNotes(e, stackTrace, 'Error during Firebase initialization');
+    // You can also log this error to Firebase Crashlytics manually
+    FirebaseCrashlytics.instance.recordError(e, stackTrace);
+  }
 
   runApp(const MyApp());
 }
@@ -36,12 +45,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Template',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeView(),
+      home: const CrashAnalyticsView(),
     );
   }
 }
